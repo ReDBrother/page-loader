b061d9be60415037b3495f2e9c67293b47b50183
Dmitriy Bataev <bataevdmitriy@gmail.com>
2017-04-19 14:49:50 +0300

    fix index-test.js

--- __tests__/index-test.js
+++ __tests__/index-test.js
@@ -10,28 +10,29 @@ nock.disableNetConnect();
 axios.defaults.adapter = httpAdapter;
 
 const tmpDir = os.tmpdir();
+const filePath1 = `${__dirname}/__fixtures__/page.html`;
+const fileData1 = fs.readFileSync(filePath1, 'utf8');
+const host = 'http://hexlet.io';
 
 describe('Loading page', () => {
   let output;
 
-  beforeEach(() => {
-    const { sep } = path;
-    output = fs.mkdtempSync(`${tmpDir}${sep}`);
-  });
-
-  it('set 1', (done) => {
-    const host = 'http://hexlet.io';
-    const status = 200;
+  beforeAll(() => {
     nock(host)
       .get('/page')
-      .replyWithFile(status, `${__dirname}/__fixtures__/page.html`);
-    
+      .replyWithFile(200, filePath1);
+  });
+
+  beforeEach(() => {
+    output = fs.mkdtempSync(`${tmpDir}${path.sep}`);
+  });
+
+  it('test loading page', (done) => {
     const pageUrl = `${host}/page`;
     loadPage(pageUrl, { output }).then((result) => {
-      fs.exists(result).then((exists) => {
-        expect(exists).toBe(true);
-        done();
-      })
-    }).catch(done.fail);
+      expect(result).toBe(`${output}${path.sep}hexlet-io-page.html`);
+      const data = fs.readFileSync(result, 'utf8');
+      expect(data).toBe(fileData1);
+    }).catch(done.fail).then(done);
   });
 });
